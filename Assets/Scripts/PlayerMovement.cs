f0a7206b3172dc1dd22925f1a96d81fe089add27
Diego <96022404+dzfrias@users.noreply.github.com>
2022-12-17 18:02:28 -0800

    feat: add movement and player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float rotateAmount;
+
+    private Rigidbody rb;
+
+    // Inputs
     private float accelerationInput;
     private float rotationInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = transform.forward * speed * 10f;
+        rb.AddForce(accelerationInput * move * Time.fixedDeltaTime, ForceMode.Acceleration);
+
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, 100 * rotationInput, 0) * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
 
     void OnAccelerate(InputValue value)
     {
