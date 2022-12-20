3d034521eaa28075db3f062e7a2ea0f024ef57db
Diego <96022404+dzfrias@users.noreply.github.com>
2022-12-20 11:39:20 -0800

    feat(car): start better movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,24 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Variables")]
+    [Header("Movement")]
     [SerializeField] private float speed;
     [SerializeField] private float rotateAmount;
+    [SerializeField] private List<Transform> wheels;
 
-    [Header("Ground Check Variables")]
+    [Header("Spring")]
+    [SerializeField] private float suspensionRest;
+    [SerializeField] private float springStrength;
+    [SerializeField] private float springDamper;
+
+    [Header("Grip")]
+    [SerializeField] private float wheelGrip;
+    [SerializeField] private float wheelMass;
+
+    [Header("Speed")]
+    [SerializeField] private float topSpeed;
+
+    [Header("Ground Check")]
     [SerializeField] private float groundCheckRadius = 0.4f;
     private LayerMask groundMask;
     private Transform groundCheck;
@@ -30,19 +43,17 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        bool isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundMask);
-        if (!isGrounded) { return; }
-
-        Vector3 move = transform.forward * speed * 10f;
-        rb.AddForce(accelerationInput * move * Time.fixedDeltaTime, ForceMode.Acceleration);
-
-        var rotate = rb.velocity.magnitude / 5;
-        if (rotate > 1f)
+        transform.Rotate(new Vector3(0, rotationInput * rotateAmount, 0));
+        foreach (Transform wheel in wheels)
         {
-            rotate = 1f;
+            RaycastHit hit;
+            if (Physics.Raycast(wheel.position, Vector3.down, out hit, 1f))
+            {
+                ApplySuspension(wheel, hit);
+                ApplySteering(wheel);
+                ApplyAcceleration(wheel);
+            }
         }
-        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, rotateAmount * rotationInput, 0) * Time.fixedDeltaTime * rotate);
-        rb.MoveRotation(rb.rotation * deltaRotation); 
     }
 
     void OnAccelerate(InputValue value)
@@ -53,5 +64,37 @@ public class PlayerMovement : MonoBehaviour
     void OnTurn(InputValue value)
     {
         rotationInput = value.Get<float>();
+    }
+
+    void ApplySuspension(Transform wheel, RaycastHit hit)
+    {
+        Vector3 springDir = wheel.up;
+        Vector3 tireVel = rb.GetPointVelocity(wheel.position);
+        float offset = suspensionRest - hit.distance;
+        float vel = Vector3.Dot(springDir, tireVel);
+        float force = (offset * springStrength) - (vel * springDamper);
+        rb.AddForceAtPosition(springDir * force, wheel.position);
+    }
+
+    void ApplySteering(Transform wheel)
+    {
+        Vector3 steeringDir = wheel.right;
+        Vector3 tireVel = rb.GetPointVelocity(wheel.position);
+        float steeringVel = Vector3.Dot(steeringDir, tireVel);
+        float velChange = -steeringVel * wheelGrip;
+        float accelChange = velChange / Time.fixedDeltaTime;
+        rb.AddForceAtPosition(steeringDir * wheelMass * accelChange, wheel.position);
+    }
+
+    void ApplyAcceleration(Transform wheel)
+    {
+        Vector3 accelDir = wheel.forward;
+        if (accelerationInput > 0f)
+        {
+            float carSpeed = Vector3.Dot(transform.forward, rb.velocity);
+            float normalSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / topSpeed);
+            float torque = 10f;
+            rb.AddForceAtPosition(accelDir * torque, wheel.position);
+        }
     }
 }
