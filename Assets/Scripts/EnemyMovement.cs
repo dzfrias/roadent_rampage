070b020b9c66d1ba1f95c0e92797363cea588f9d
Tanner <tannerelephant@gmail.com>
2022-12-22 00:05:57 -0800

    feat(enemy): basic movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private Transform player;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
