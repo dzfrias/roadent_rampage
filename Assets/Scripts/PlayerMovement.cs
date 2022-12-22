e7b91a8caecec04e05ea1d2623d4912316d81de5
Diego <96022404+dzfrias@users.noreply.github.com>
2022-12-21 17:11:28 -0800

    feat: make a semi-drivable car

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public List<Wheel> wheels;
+
+    [Header("Specs")]
+    public float wheelBase;
+    public float rearTrack;
+    public float turnRadius;
+
+    // Inputs
+    private float rotationInput;
+    private float accelerationInput;
+
+    private float angleLeft;
+    private float angleRight;
+
+    void Update()
+    {
+        if (rotationInput > 0)
+        {
+            angleLeft  = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * rotationInput;
+            angleRight = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius - (rearTrack / 2))) * rotationInput;
+        } else if (rotationInput < 0)
+        {
+            angleLeft  = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius - (rearTrack / 2))) * rotationInput;
+            angleRight = Mathf.Rad2Deg * Mathf.Atan(wheelBase / (turnRadius + (rearTrack / 2))) * rotationInput;
+        } else
+        {
+            angleLeft  = 0;
+            angleRight = 0;
+        }
+
+        foreach (Wheel wheel in wheels)
+        {
+            if (wheel.corner == Corner.FrontLeft)
+            {
+                wheel.steerAngle = angleLeft;
+            }
+            if (wheel.corner == Corner.FrontRight)
+            {
+                wheel.steerAngle = angleRight;
+            }
+            wheel.accelerationInput = accelerationInput;
+        }
+    }
+
+    void OnTurn(InputValue value)
+    {
+        rotationInput = value.Get<float>();
+    }
+
+    void OnAccelerate(InputValue value)
+    {
+        accelerationInput = value.Get<float>();
+    }
+}
--- Assets/Scripts/Wheel.cs
+++ Assets/Scripts/Wheel.cs
@@ -2,44 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Corner
+{
+    FrontLeft,
+    FrontRight,
+    BackLeft,
+    BackRight,
+}
+
 public class Wheel : MonoBehaviour
 {
     private Rigidbody rb;
 
     [Header("Suspension")]
     [SerializeField] private float restLength;
-    [SerializeField] private float springTravel;
     [SerializeField] private float springStiffness;
     [SerializeField] private float damperStiffness;
 
-    private float minLength;
-    private float maxLength;
-    private float springLength;
-
     [Header("Wheel")]
+    public Corner corner;
     [SerializeField] private float radius;
+
+    [SerializeField] private float steerTime;
+    [System.NonSerialized] public float steerAngle;
+    private float wheelAngle;
+
+    [System.NonSerialized] public float accelerationInput;
 
     void Start()
     {
         rb = transform.root.GetComponent<Rigidbody>();
+    }
 
-        minLength = restLength - springTravel;
-        maxLength = restLength + springTravel;
+    void Update()
+    {
+        wheelAngle = Mathf.Lerp(wheelAngle, -steerAngle, steerTime * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(Vector3.up * wheelAngle);
     }
 
     void FixedUpdate()
     {
-        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxLength + radius))
+
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, radius))
         {
-            float lastLength = springLength;
-            springLength = Mathf.Clamp(hit.distance - radius, minLength, maxLength);
-            float springVelocity = (lastLength - springLength) / Time.fixedDeltaTime;
-            float springForce = springStiffness * (restLength - springLength);
-            float damperForce = damperStiffness * springVelocity;
+            Vector3 springDir = transform.up;
+            Vector3 tireVel = rb.GetPointVelocity(transform.position);
+            float offset = restLength - hit.distance;
+            float vel = Vector3.Dot(springDir, tireVel);
+            float force = (offset * springStiffness) - (vel * damperStiffness);
 
-            Vector3 suspensionForce = (springForce + damperForce) * transform.up;
+            Vector3 wheelVelocity = transform.InverseTransformDirection(rb.GetPointVelocity(hit.point));
+            float fx = accelerationInput * force;
+            float fy = wheelVelocity.x  * force;
 
-            rb.AddForceAtPosition(suspensionForce, hit.point);
+            rb.AddForceAtPosition((springDir * force) + (fx * transform.forward) + (fy * -transform.right), transform.position);
         }
     }
 }
