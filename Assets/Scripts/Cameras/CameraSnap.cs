c1a7abed803b2d4d8db8a1d95a912becf38c4a06
Diego <96022404+dzfrias@users.noreply.github.com>
2022-12-23 22:50:28 -0800

    feat(camera): smooth main/snap camera transitions

--- Assets/Scripts/Cameras/CameraSnap.cs
+++ Assets/Scripts/Cameras/CameraSnap.cs
@@ -7,7 +7,6 @@ using Cinemachine;
 public class CameraSnap : MonoBehaviour
 {
     [SerializeField] private PlayerTrigger detectionCollider;
-    [SerializeField] private float blendTime = 0.5f;
     [SerializeField] private int setPriority = 100;
     private CinemachineBrain brain;
     private CinemachineVirtualCamera vcam;
@@ -25,20 +24,13 @@ public class CameraSnap : MonoBehaviour
 
     void Update()
     {
-        if (!colliding)
+        if (detectionCollider.isColliding)
         {
-            brain.m_DefaultBlend.m_Time = startBlendTime;
-        }
-        if (detectionCollider.isColliding && !colliding)
-        {
-            brain.m_DefaultBlend.m_Time = blendTime;
             vcam.Priority = setPriority;
-            colliding = true;
         }
         else if (!detectionCollider.isColliding)
         {
             vcam.Priority = startPriority;
-            colliding = false;
         }
     }
 }
