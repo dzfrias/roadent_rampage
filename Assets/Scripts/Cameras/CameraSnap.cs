66f8493e0ab15bcdb5b2c5429dce7b54b4c29738
Diego <96022404+dzfrias@users.noreply.github.com>
2022-12-23 22:51:54 -0800

    chore(camera): remove unused fields

--- Assets/Scripts/Cameras/CameraSnap.cs
+++ Assets/Scripts/Cameras/CameraSnap.cs
@@ -8,18 +8,11 @@ public class CameraSnap : MonoBehaviour
 {
     [SerializeField] private PlayerTrigger detectionCollider;
     [SerializeField] private int setPriority = 100;
-    private CinemachineBrain brain;
     private CinemachineVirtualCamera vcam;
-    private int startPriority;
-    private float startBlendTime;
-    private bool colliding;
 
     void Start()
     {
         vcam = GetComponent<CinemachineVirtualCamera>();
-        brain = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineBrain>();
-        startPriority = vcam.Priority;
-        startBlendTime = brain.m_DefaultBlend.m_Time;
     }
 
     void Update()
