486cbbbcaf438f0c25a9756dd7192c42aa453b6e
Diego <96022404+dzfrias@users.noreply.github.com>
2022-12-30 12:56:21 -0800

    refactor(finish): stop timer using events

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
-    private Timer timer;
-    
-    void Start()
-    {
-        timer = FindObjectOfType<Timer>();
-    }
-
     public void FinishLevel() 
     {
         Debug.Log("Player Finished The Level!");
-        timer?.Stop();
     }
 }
