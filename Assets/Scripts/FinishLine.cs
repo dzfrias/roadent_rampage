b16b4c347a05200ec2f25d4e258ad5b2a98d9632
Tanner <tannerelephant@gmail.com>
2022-12-30 11:40:29 -0800

    feat(timer): add timer script and ui

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
+    private Timer timer;
+    
+    void Start()
+    {
+        timer = FindObjectOfType<Timer>();
+    }
+
     public void FinishLevel() 
     {
         Debug.Log("Player Finished The Level!");
+        timer?.Stop();
     }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text timerText;
+    
+    private int minutes;
+    private float seconds;
+    private bool counting = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!counting) { return; }
+        seconds += Time.deltaTime;
+        if (seconds >= 60f)
+        {
+            minutes += 1;
+            seconds = 0f;
+        }
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00.00");
+    }
+
+    public void Stop()
+    {
+        counting = false;
+    }
+}
