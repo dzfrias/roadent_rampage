13b5cf8f713cf3643e682ad629cf09d9948dcee2
Diego <96022404+dzfrias@users.noreply.github.com>
2023-01-08 14:09:25 -0800

    feat(player): smoother shooting controls

--- Assets/Scripts/Player/MouseAim.cs
+++ Assets/Scripts/Player/MouseAim.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class MouseAim : MonoBehaviour
 {
-    [SerializeField] private float rotateSpeed = 400f;
+    [SerializeField] private float rotateSpeed = 300f;
+    [SerializeField] private float rotateMultiplier = 0.99f;
+    private Vector3 rotate;
 
     void Start()
     {
@@ -14,6 +16,14 @@ public class MouseAim : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rotateSpeed);
+        if (Input.GetAxis("Mouse Y") == 0 && Input.GetAxis("Mouse X") == 0)
+        {
+            rotate *= rotateMultiplier;
+        }
+        else
+        {
+            rotate = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * rotateSpeed;
+        }
+        transform.Rotate(rotate * Time.deltaTime);
     }
 }
