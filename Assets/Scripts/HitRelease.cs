453c97106001448112abd8ee7014999f2394e1f7
Diego <96022404+dzfrias@users.noreply.github.com>
2023-01-28 14:44:41 -0800

    feat(obstacle): add particle effects

---
+++ Assets/Scripts/HitRelease.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitRelease : MonoBehaviour, IHittable
+{
+    [SerializeField] private GameObject particles;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject p = Instantiate(particles, collision.contacts[0].point, Quaternion.identity);
+        p.GetComponent<ParticleSystem>().Play();
+    }
+
+    public void Hit(Vector3 hitPoint, Vector3 direction)
+    {
+        GameObject p = Instantiate(particles, hitPoint, Quaternion.LookRotation(direction));
+        p.GetComponent<ParticleSystem>().Play();
+    }
+}
