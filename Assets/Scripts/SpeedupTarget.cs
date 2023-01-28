523face63e0414cb3cb10c97251a3ded93c30014
Diego <96022404+dzfrias@users.noreply.github.com>
2023-01-28 09:23:25 -0800

    feat(target): change color on hit

--- Assets/Scripts/SpeedupTarget.cs
+++ Assets/Scripts/SpeedupTarget.cs
@@ -7,13 +7,18 @@ public class SpeedupTarget : MonoBehaviour, IHittable
     [SerializeField] private float speedForce = 30f;
     [SerializeField] private float hitCooldown = 2f;
     [SerializeField] private GameObject particles;
+    [SerializeField] private Color hitColor;
 
     private bool canBeHit = true;
+    private Material material;
+    private Color original;
     private GameObject player;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        material = GetComponent<Renderer>().material;
+        original = material.color;
     }
 
     public void Hit(Vector3 hitPoint, Vector3 direction)
@@ -28,7 +33,9 @@ public class SpeedupTarget : MonoBehaviour, IHittable
     IEnumerator HitCooldown()
     {
         canBeHit = false;
+        material.color = hitColor;
         yield return new WaitForSeconds(hitCooldown);
+        material.color = original;
         canBeHit = true;
     }
 }
