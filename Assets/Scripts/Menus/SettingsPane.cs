c1d5c38f939d163147ad8facbffa4a32b1685000
Diego <96022404+dzfrias@users.noreply.github.com>
2023-02-04 08:51:45 -0800

    feat(ui): add screen settings options

--- Assets/Scripts/Menus/SettingsPane.cs
+++ Assets/Scripts/Menus/SettingsPane.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using TMPro;
 
 public class SettingsPane : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        resolutionDropdown.AddOptions(resolutions.Select(res => res.width + " x " + res.height).ToList());
+        resolutionDropdown.value = Array.FindIndex(
+                resolutions,
+                res => res.width == Screen.currentResolution.width && res.height == Screen.currentResolution.height
+        );
+    }
 
     public void SetVolume(float volume)
     {
@@ -15,6 +32,17 @@ public class SettingsPane : MonoBehaviour
     public void SetQuality(int qualityIndex)
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
     }
 
     void Update()
