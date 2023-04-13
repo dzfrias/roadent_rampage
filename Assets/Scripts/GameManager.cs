45ba9fb7323b70077fdbab054d77756173a6e8f0
Diego <96022404+dzfrias@users.noreply.github.com>
2023-04-12 18:41:58 -0700

    chore(menu): load after game completion

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -49,6 +50,7 @@ public class GameManager : MonoBehaviour
     public void Finish()
     {
         onFinishLineReached?.Invoke();
+        SceneManager.LoadScene(0);
         Debug.Log("Player Finished The Level!");
     }
 }
