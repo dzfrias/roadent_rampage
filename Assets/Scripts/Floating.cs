48e1068fee53e74b18780a03406423f082826951
Diego <96022404+dzfrias@users.noreply.github.com>
2023-04-15 08:32:55 -0700

    chore(Floating): allow it to be enabled in scene

--- Assets/Scripts/Floating.cs
+++ Assets/Scripts/Floating.cs
@@ -10,15 +10,26 @@ public class Floating : MonoBehaviour
     [SerializeField] private float spawnMoveAmount;
     [SerializeField] private float spawnMoveDuration = 0.8f;
 
+    private float startX;
+    private bool firstEnable;
+
     void Start()
     {
         float startPos = transform.position.y;
         transform.DOMoveY(startPos + moveAmount, moveDuration).SetEase(Ease.OutBounce).SetLoops(-1, LoopType.Yoyo).SetId(gameObject);
+        startX = transform.localPosition.x;
+        gameObject.SetActive(false);
+        transform.Translate(new Vector3(-spawnMoveAmount, 0, 0), Space.World);
     }
 
     void OnEnable()
     {
-        transform.DOMoveX(transform.position.x + spawnMoveAmount, spawnMoveDuration).SetEase(Ease.OutElastic);
+        if (!firstEnable)
+        {
+            firstEnable = true;
+            return;
+        }
+        transform.DOLocalMoveX(startX, spawnMoveDuration).SetEase(Ease.OutElastic);
     }
 
     void OnDestroy()
