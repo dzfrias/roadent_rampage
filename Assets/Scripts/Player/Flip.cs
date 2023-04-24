885812596ab3dc43900bf07e1559ffcd7378aaa6
Diego <96022404+dzfrias@users.noreply.github.com>
2023-04-24 14:39:58 -0700

    fix(player): finally fix unflip mechanics

--- Assets/Scripts/Player/Flip.cs
+++ Assets/Scripts/Player/Flip.cs
@@ -2,52 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Collider), typeof(IMover))]
 public class Flip : MonoBehaviour
 {
-    [SerializeField] private float unflipTime;
-    [SerializeField] private float upsideDownEpsilon = 0.01f;
-    [SerializeField] private float rightSideEpsilon = 0.01f;
-    private float flippedTime;
-    private bool flipped;
+    private IMover mover;
+    private bool isTouching;
 
-    void OnCollisionEnter(Collision collision)
+    void Start()
     {
-        if (collision.gameObject.layer == 3)
-        {
-            flipped = true;
-        }
+        mover = GetComponent<IMover>();
     }
 
-    void OnCollisionExit(Collision collision)
+    void OnCollisionEnter(Collision _)
     {
-        if (collision.gameObject.layer == 3)
-        {
-            flipped = false;
-            flippedTime = 0f;
-        }
+        isTouching = true;
     }
 
-    void Update()
+    void OnCollisionExit(Collision _)
     {
-        if (flipped)
-        {
-            flippedTime += Time.deltaTime;
-        }
+        isTouching = false;
     }
 
     public void Unflip()
     {
-        if (flippedTime >= unflipTime)
+        if (isTouching && !mover.IsGrounded())
         {
-            if (transform.up.y < -1 + upsideDownEpsilon)
-            {
-                transform.rotation = transform.rotation * Quaternion.AngleAxis(180, Vector3.forward);
-            }
-            else if (transform.up.y > 0 - rightSideEpsilon && transform.up.y < 0 + rightSideEpsilon)
-            {
-                transform.rotation = transform.rotation * Quaternion.AngleAxis(-90, Vector3.forward);
-            }
+            transform.rotation = Quaternion.identity;
         }
     }
 }
