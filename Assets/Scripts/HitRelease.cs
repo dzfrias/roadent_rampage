33ea5502a8595cce684b1d674fbe5246ca70eba5
Diego <96022404+dzfrias@users.noreply.github.com>
2023-04-24 19:04:51 -0700

    chore(shooter): distinguish important targets

--- Assets/Scripts/HitRelease.cs
+++ Assets/Scripts/HitRelease.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class HitRelease : MonoBehaviour, IHittable
 {
     [SerializeField] private GameObject particles;
+    [SerializeField] private bool playSound = true;
 
     public void Hit(Vector3 hitPoint, Vector3 direction)
     {
-        AudioManager.instance.Play("hit");
+        if (playSound) AudioManager.instance.Play("hit");
         GameObject p = Instantiate(particles, hitPoint, Quaternion.LookRotation(direction));
         p.GetComponent<ParticleSystem>().Play();
     }
