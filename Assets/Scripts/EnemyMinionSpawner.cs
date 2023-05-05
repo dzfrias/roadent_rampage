b365cb635879b4d89591a422e8766f5d0e0b5fe2
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-04 21:18:47 -0700

    chore(enemy): improve minion spawning

--- Assets/Scripts/EnemyMinionSpawner.cs
+++ Assets/Scripts/EnemyMinionSpawner.cs
@@ -1,28 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyMinionSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyMinionPrefab;
+    [SerializeField] private float maxOffset;
+    [SerializeField] private Vector2 spawnBetween = new Vector2(10, 15);
+    [SerializeField] private Vector2 distBetween = new Vector2(30, 50);
+    [SerializeField] private int maxSpawn = 3;
 
-    [SerializeField] private float spawnrate = 10f;
-
-    [SerializeField] private Vector3 spawnLocationFromEnemy;
-
-    private EnemyMovement enemyMovement;
+    private int hasSpawned;
 
     void Start()
     {
-        enemyMovement = GetComponent<EnemyMovement>();
-        InvokeRepeating("SpawnEnemyMinion", 0f, spawnrate);
+        StartCoroutine(Spawn());
     }
 
-    private void SpawnEnemyMinion()
+    void OnValidate()
     {
-        Vector3 enemySpawnLocation = transform.position + spawnLocationFromEnemy;
-        GameObject enemyMinion = Instantiate(enemyMinionPrefab, enemySpawnLocation, Quaternion.identity);
+        // Ensure y is equal to or greater than x
+        spawnBetween = new Vector2(spawnBetween.x, Mathf.Max(spawnBetween.x, spawnBetween.y));
+        distBetween = new Vector2(distBetween.x, Mathf.Max(distBetween.x, distBetween.y));
+    }
+
+    IEnumerator Spawn()
+    {
+        float timer = Random.Range(spawnBetween.x, spawnBetween.y);
+        while (true)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0 && hasSpawned < maxSpawn)
+            {
+                timer = Random.Range(spawnBetween.x, spawnBetween.y);
+                SpawnEnemyMinion();
+                hasSpawned += 1;
+            }
+            yield return null;
+        }
+    }
+
+    void SpawnEnemyMinion()
+    {
+        Vector3 enemySpawn = (transform.position + (transform.forward * Random.Range(distBetween.x, distBetween.y))) + Vector3.right * Random.Range(-maxOffset, maxOffset);
+        GameObject enemyMinion = Instantiate(enemyMinionPrefab, transform.position, Quaternion.identity);
         EnemyMovement minionMovement = enemyMinion.GetComponent<EnemyMovement>();
-        minionMovement.SetTarget(enemyMovement.GetTarget());
+        minionMovement.SetTarget(transform);
+
+        // Move to closest point in navmesh
+        NavMeshHit hit;
+        NavMesh.SamplePosition(enemySpawn, out hit, Mathf.Infinity, NavMesh.AllAreas);
+        enemyMinion.GetComponent<NavMeshAgent>().Warp(hit.position);
     }
 }
---
+++ Assets/Scripts/Inflate.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Inflate : MonoBehaviour
+{
+    [SerializeField] private float duration = 1f;
+    [SerializeField] private bool onStart;
+    [SerializeField] private bool lockPosition = true;
+
+    void Start()
+    {
+        if (!onStart)
+        {
+            return;
+        }
+        ActivateInflate();
+    }
+
+    public void ActivateInflate()
+    {
+        Vector3 endScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+        transform.DOScale(endScale, duration).SetEase(Ease.OutQuad);
+        if (lockPosition) StartCoroutine(LockPosition());
+    }
+
+    IEnumerator LockPosition()
+    {
+        Vector3 startPos = transform.position;
+        float d = duration;
+        while (d > 0)
+        {
+            transform.position = startPos;
+            d -= Time.deltaTime;
+            yield return null;
+        }
+    }
+}
