79345db6475dc31a3cf5de2c63aa1641be969493
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-06 12:50:11 -0700

    fix(tutorial): add responsive text prompts

--- Assets/Scripts/KeyText.cs
+++ Assets/Scripts/KeyText.cs
@@ -1,19 +1,15 @@
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class KeyText : MonoBehaviour
 {
-    [SerializeField] private List<string> keys;
     [SerializeField] private string prompt;
     [SerializeField] private float removeAfter = 2.5f;
 
-    private bool active;
-
     public void Activate()
     {
-        active = true;
         GameManager.instance.BroadcastText(prompt);
         Time.timeScale = 0.5f;
         GameManager.instance.Darken();
@@ -22,26 +18,14 @@ public class KeyText : MonoBehaviour
 
     public void Deactivate()
     {
-        active = false;
         GameManager.instance.ClearBroadcast();
         GameManager.instance.Undarken();
         Time.timeScale = 1f;
     }
 
-    void Update()
-    {
-        if (!active) return;
-
-        if (keys.Any(key => Input.GetKeyDown(key)))
-        {
-            Deactivate();
-        }
-    }
-
     IEnumerator Remove()
     {
         yield return new WaitForSecondsRealtime(removeAfter);
-        if (!active) yield break;
         Deactivate();
     }
 }
--- Assets/Scripts/TextBroadcast.cs
+++ Assets/Scripts/TextBroadcast.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,45 @@ using TMPro;
 public class TextBroadcast : MonoBehaviour
 {
     private TMP_Text text;
+
+    public static string InterpolatePrompt(string prompt)
+    {
+        PlayerInput input = PlayerInput.all[0];
+        StringBuilder sb = new StringBuilder(prompt);
+        if (input.currentControlScheme == "TwoGamepads")
+        {
+            Gamepad gamepad = Gamepad.current;
+            string scheme;
+            if (gamepad is UnityEngine.InputSystem.XInput.XInputController)
+            {
+                scheme = "Xbox Prompts";
+            }
+            else if (gamepad is UnityEngine.InputSystem.DualShock.DualShockGamepad)
+            {
+                scheme = "PS5 Prompts";
+            }
+            else
+            {
+                scheme = "?";
+            }
+            
+            sb.Replace("\\Forward", $"<sprite=\"{scheme}\" name=\"Left Stick\">");
+            sb.Replace("\\Turn", $"<sprite=\"{scheme}\" name=\"Left Stick\">");
+            sb.Replace("\\Shoot", $"<sprite=\"{scheme}\" name=\"Right Trigger\">");
+            sb.Replace("\\Flip", $"<sprite=\"{scheme}\" name=\"South\">");
+            sb.Replace("\\Aim", $"<sprite=\"{scheme}\" name=\"Right Stick\">");
+        }
+        else
+        {
+            sb.Replace("\\Forward", "<sprite=\"KBM Prompts\" name=\"W\">");
+            sb.Replace("\\Shoot", "<sprite=\"KBM Prompts\" name=\"Space\">");
+            sb.Replace("\\Flip", "<sprite=\"KBM Prompts\" name=\"F\">");
+            sb.Replace("\\Aim", "mouse");
+            sb.Replace("\\Turn", "<sprite=\"KBM Prompts\" name=\"Left\"><sprite=\"KBM Prompts\" name=\"Right\">");
+        }
+
+        return sb.ToString();
+    }
 
     void Awake()
     {
@@ -23,7 +64,8 @@ public class TextBroadcast : MonoBehaviour
 
     void ShowText(string newText)
     {
-        text.text = newText;
+        string t = InterpolatePrompt(newText);
+        text.text = t;
     }
 
     void ClearText()
