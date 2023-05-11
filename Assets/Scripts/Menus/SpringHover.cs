d7ae37dc71cd49e855c8c8e2ccf2eadf45142a49
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-10 19:22:54 -0700

    refactor(SpringHover): serialize fields

--- Assets/Scripts/Menus/SpringHover.cs
+++ Assets/Scripts/Menus/SpringHover.cs
@@ -6,8 +6,13 @@ using UnityEngine.EventSystems;
 using DG.Tweening;
 
 [RequireComponent(typeof(Button))]
-public class SpringHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler
+public class SpringHover : MonoBehaviour, IPointerEnterHandler, ISelectHandler
 {
+    [SerializeField] private Vector3 punch = new Vector3(1f, 1f, 0f);
+    [SerializeField] private float punchDuration = 0.5f;
+    [SerializeField] private int punchElasticity = 6;
+    [SerializeField] private float punchDampening = 0.2f;
+
     private Button button;
 
     void Start()
@@ -20,23 +25,10 @@ public class SpringHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         button.Select();
     }
 
-    public void OnPointerExit(PointerEventData _)
-    {
-        var current = EventSystem.current;
-        if (current == null) return;
-
-        if (EventSystem.current.currentSelectedGameObject != gameObject)
-        {
-            return;
-        }
-        // Deselect
-        EventSystem.current.SetSelectedGameObject(null);
-    }
-
     public void OnSelect(BaseEventData _)
     {
         DOTween.Complete(gameObject);
-        transform.DOPunchScale(new Vector3(1f, 1f, 0f), 0.5f, 6, 0.2f).SetId(gameObject);
+        transform.DOPunchScale(punch, punchDuration, punchElasticity, punchDampening).SetId(gameObject);
     }
 
     void OnDestroy()
