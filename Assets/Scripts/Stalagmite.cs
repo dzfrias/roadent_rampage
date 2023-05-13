e5dd9586f9f2fde58b8d2922aea78556274d479b
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-13 08:28:25 -0700

    feat(stalagmite): improve
    Use more physics-realistic easing function for falling, and distinguish
    between the `falling` and not falling states. Only falling stalagmites
    should restart the player.

--- Assets/Scripts/Stalagmite.cs
+++ Assets/Scripts/Stalagmite.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Events;
 
 public class Stalagmite : MonoBehaviour
 {
     [SerializeField] private float distance;
     [SerializeField] private float time = 0.75f;
+    [SerializeField] private UnityEvent onFallingHit;
+
+    private bool falling;
 
     public void Activate()
     {
-        transform.DOMoveY(transform.position.y - distance, time);
+        falling = true;
+        transform
+            .DOMoveY(transform.position.y - distance, time)
+            .SetEase(Ease.InQuad)
+            .OnComplete(() => falling = false);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        if (falling)
+        {
+            onFallingHit?.Invoke();
+        }
     }
 }
