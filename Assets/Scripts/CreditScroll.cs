b3ab194563f36474ab6c2c5b4468dad36186b4ed
Diego <96022404+dzfrias@users.noreply.github.com>
2023-05-19 22:44:51 -0700

    feat(credits): integrate with game

--- Assets/Scripts/CreditScroll.cs
+++ Assets/Scripts/CreditScroll.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using TMPro;
 
 public class CreditScroll : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float completedAfter = 100f;
+    [SerializeField] private UnityEvent onComplete;
+
     private RectTransform rectTransform;
 
     void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        Time.timeScale = 1f;
     }
 
     void Update()
     {
+        if (rectTransform.position.y > completedAfter)
+        {
+            onComplete?.Invoke();
+            enabled = false;
+        }
         rectTransform.Translate(Vector3.up * Time.deltaTime * moveSpeed);
     }
 }
